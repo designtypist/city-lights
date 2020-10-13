bab3e03a60c4ac83a248596aa8240ae159aef460
James Chung <designtypist@gmail.com>
2020-10-13 18:01:46 -0400

    made the mobile navigation menu button functional

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,51 @@ import { Link } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default () => (
-  <nav role="navigation" class="flex content-start justify-between flex-wrap sm:m-6 xl:m-2">
+  <nav role="navigation" class="flex flex-wrap content-start xl:m-2 sm:m-6">
     <div class="flex-initial w-1/3">
       <Link to="/">
         <img class="w-56"  src={'../citylights-logo.png'} alt="City Lights Logo" />
       </Link>
     </div>
-    <div class="flex-initial w-2/3 text-right md:hidden">
-      <input id="nav-menu" class="hidden" type="checkbox" />
+
+    <div class="flex-initial w-2/3 text-right md:hidden p-4">
       <label class="flex-initial items-center px-3 py-2 border rounded text-black border-black" htmlFor="nav-menu" for="nav-menu">
         <FontAwesomeIcon icon={['fa', 'bars']} />
       </label>
     </div>
-    <div class="flex-initial md:w-1/3 w-full text-center">
+    <input class="hidden" id="nav-menu" type="checkbox" />
+    <div class="mobile-nav hidden w-full text-center text-black uppercase">
+      <ul>
+        <li class="text-3xl my-8">
+          <Link to="/">Home</Link>
+        </li>
+        <li class="text-3xl my-8">
+          <Link to="/media/">Media</Link>
+        </li>
+        <li class="text-3xl my-8">
+          <Link to="/contact/">Contact</Link>
+        </li>
+      </ul>
+      <ul>
+        <li class="inline-block">
+          <Link class="text-3xl px-2" to="#">
+            <FontAwesomeIcon icon={['fab', 'youtube']} />
+          </Link>
+        </li>
+        <li class="inline-block">
+          <Link class="text-3xl px-2" to="#">
+            <FontAwesomeIcon icon={['fab', 'instagram']} />
+          </Link>
+        </li>
+        <li class="inline-block">
+          <Link class="text-3xl px-2" to="#">
+            <FontAwesomeIcon icon={['fab', 'facebook']} />
+          </Link>
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex-initial md:w-1/3 md:text-center md:inline-block hidden uppercase">
       <ul class="text-black text-base md:mt-4 sm:mt:0">
         <li class="md:inline-block md:mr-4 md:my-0 sm:my-2">
           <Link to="/">Home</Link>
@@ -28,16 +60,24 @@ export default () => (
         </li>
       </ul>
     </div>
-    <div class="flex-initial md:w-1/3 sm:w-full md:text-right sm:text-center p-4">
-      <Link class="text-2xl px-2" to="#">
-          <FontAwesomeIcon icon={['fab', 'youtube']} />
-      </Link>
-      <Link class="text-2xl px-2" to="#">
-        <FontAwesomeIcon icon={['fab', 'instagram']} />
-      </Link>
-      <Link class="text-2xl px-2" to="#">
-        <FontAwesomeIcon icon={['fab', 'facebook']} />
-      </Link>
+    <div class="flex-initial md:w-1/3 md:text-right md:inline-block hidden uppercase">
+      <ul class="p-4">
+        <li class="inline-block">
+          <Link class="text-2xl px-2" to="#">
+            <FontAwesomeIcon icon={['fab', 'youtube']} />
+          </Link>
+        </li>
+        <li class="inline-block">
+          <Link class="text-2xl px-2" to="#">
+            <FontAwesomeIcon icon={['fab', 'instagram']} />
+          </Link>
+        </li>
+        <li class="inline-block">
+          <Link class="text-2xl px-2" to="#">
+            <FontAwesomeIcon icon={['fab', 'facebook']} />
+          </Link>
+        </li>
+      </ul>
     </div>
   </nav>
 )
